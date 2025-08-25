d4c3592eabcb83924399580bd6a6b6cbd5985241
Nicolas Moura <nicolasciuffi1@gmail.com>
2025-08-24 23:17:04 -0300

    Melhoria na mensagem de sucesso ao rodar a aplicação

--- src/main/java/br/gov/caixa/hackaton/HackatonApplication.java
+++ src/main/java/br/gov/caixa/hackaton/HackatonApplication.java
@@ -8,6 +8,10 @@ public class HackatonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackatonApplication.class, args);
+		System.out.println("✅ Aplicação iniciada com sucesso!");
+		System.out.println("Url base da aplicação: http://localhost:8080");
+		System.out.println("Url da documentação swagger da aplicação: http://localhost:8080/swagger-ui/index.html");
+		System.out.println("Url da collection postman da aplicação: http://localhost:8080/collection");
 	}
 
 }
